c0c033c660e271fc331eacda3b60b4aa9157c806
dev8523 <debasishsahoo8523@gmail.com>
2021-10-06 23:19:33 +0530

    Prints two strings in single line.

---
+++ src/com/debasish/practise/PrintStringsInSingleLine.java
@@ -0,0 +1,13 @@
+package com.debasish.practise;
+
+/**
+ * Print two strings in single line.
+ *
+ * @author debasish.sahoo
+ */
+public class PrintStringsInSingleLine {
+
+    public static void main(String[] args) {
+        System.out.print("Hello " + " World!");
+    }
+}
