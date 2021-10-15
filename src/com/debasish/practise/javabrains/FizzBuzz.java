a610beae4da17a45ef3f48ad04af2cc24c2baf87
dev8523 <debasishsahoo8523@gmail.com>
2021-10-15 23:46:20 +0530

    Find and Print FizzBuzz for each num in the input

--- src/com/debasish/practise/javabrains/FizzBuzz.java
+++ src/com/debasish/practise/javabrains/FizzBuzz.java
@@ -8,16 +8,37 @@ import java.util.Scanner;
 public class FizzBuzz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Your Input : ");
+        System.out.println(":: Your Input ::");
         findFizzBuzz(scanner.nextInt());
+        System.out.println("\n:: Your Input to check for each number in the input ::");
+        printFizzBuzzForEachNumInInput(scanner.nextInt());
     }
 
     private static void findFizzBuzz(int input) {
-        if (input % 3 == 0 && input % 5 == 0)
+        if ((input % 3 == 0) && (input % 5 == 0))
             System.out.println("FizzBuzz");
         else if (input % 3 == 0)
             System.out.println("Fizz");
         else if (input % 5 == 0)
             System.out.println("Buzz");
+        else
+            System.out.println("Neither a multiple of 3 nor 5 !!");
+    }
+
+    private static void printFizzBuzzForEachNumInInput(int input) {
+        for (int num = 0; num <= input; num++) {
+            if ((num % 3 == 0) && (num % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            else if (num % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (num % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(num);
+            }
+        }
     }
 }
