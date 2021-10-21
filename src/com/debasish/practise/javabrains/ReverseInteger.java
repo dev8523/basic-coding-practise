e9963688181a8b09d08a07e403f41b1960ef7d0f
dev8523 <debasishsahoo8523@gmail.com>
2021-10-21 22:43:42 +0530

    Algorithm for Reversing an Integer

--- src/com/debasish/practise/javabrains/ReverseInteger.java
+++ src/com/debasish/practise/javabrains/ReverseInteger.java
@@ -1,2 +1,20 @@
-package com.debasish.practise.javabrains;public class ReverseInteger {
+package com.debasish.practise.javabrains;
+
+/**
+ * @author debasishsahoo
+ */
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        System.out.println(reverseInteger(539901123));
+    }
+
+    private static int reverseInteger(int input) {
+        int reverse = 0;
+        while (input != 0) {
+            reverse = reverse * 10 + input % 10;
+            input /= 10;
+        }
+        return reverse;
+    }
 }
