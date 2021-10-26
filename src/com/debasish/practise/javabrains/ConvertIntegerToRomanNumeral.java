3e61fb0563b217c63f91e7af8a48dfb6c27d8ecc
dev8523 <debasishsahoo8523@gmail.com>
2021-10-27 00:24:03 +0530

    Add RuntimeException and use scanner input for IntToRoman conversion

--- src/com/debasish/practise/javabrains/ConvertIntegerToRomanNumeral.java
+++ src/com/debasish/practise/javabrains/ConvertIntegerToRomanNumeral.java
@@ -3,13 +3,18 @@
  */
 package com.debasish.practise.javabrains;
 
+import java.util.Scanner;
+
 /**
  * @author debasishsahoo
  */
 public class ConvertIntegerToRomanNumeral {
     public static void main(String[] args) {
-        int num = 123;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input (0 < input < 4k): ");
+        int num = scanner.nextInt();
         System.out.println("Roman numeral for " + num + " : " + intToRoman(num));
+        scanner.close();
     }
 
     private static String intToRoman(int num) {
@@ -18,9 +23,12 @@ public class ConvertIntegerToRomanNumeral {
         String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}; // "", 100, 200, 300, ..., 900
         String[] thousands = {"", "M", "MM", "MMM"}; // "", 1000, 2000, 3000, 4000
 
-        return thousands[num / 1000] +
-                hundreds[(num % 1000) / 100] +
-                tens[(num % 100) / 10] +
-                units[(num % 10)];
+        if (num < 4000)
+            return thousands[num / 1000] +
+                    hundreds[(num % 1000) / 100] +
+                    tens[(num % 100) / 10] +
+                    units[(num % 10)];
+        else
+            throw new RuntimeException("Given input is not in range of the algorithm!! Try any input from 0 to 3999.");
     }
 }
