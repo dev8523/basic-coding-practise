a537be5dad7cf130385e5a65e6453a9bcbca6c74
dev8523 <debasishsahoo8523@gmail.com>
2021-10-27 21:17:35 +0530

    Update comments for minmax

--- src/com/debasish/practise/final450/FindMinAndMaxOfAnArray.java
+++ src/com/debasish/practise/final450/FindMinAndMaxOfAnArray.java
@@ -1,17 +1,26 @@
-/**
- * WAP to return minimum and maximum in an array.
- * Your program should make the minimum number of comparisons.
- */
 package com.debasish.practise.final450;
 
 import java.util.Arrays;
 
+/**
+ * WAP to return minimum and maximum in an array.
+ * Your program should make the minimum number of comparisons.
+ *
+ * @author debasishsahoo
+ */
 public class FindMinAndMaxOfAnArray {
     public static void main(String[] args) {
         int[] arr = {-1, 3, 0, 11, 1, -9};
-        System.out.println("[Min, Max] element: " + Arrays.toString(findMin(arr)));
+        System.out.println("[Min, Max] : " + Arrays.toString(findMin(arr)));
     }
 
+    /**
+     * Time Complexity: O(n)
+     * Space Complexity: O(1)
+     *
+     * @param arr pass input array
+     * @return int[]
+     */
     private static int[] findMin(int[] arr) {
         int min = arr[0];
         int max = arr[0];
