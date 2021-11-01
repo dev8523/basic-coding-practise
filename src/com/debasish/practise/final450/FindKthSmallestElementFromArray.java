d2bc544922480a37b09c792edc69ac9a98c32d3a
dev8523 <debasishsahoo8523@gmail.com>
2021-11-01 15:49:19 +0530

    Update javadoc for understanding

--- src/com/debasish/practise/final450/FindKthSmallestElementFromArray.java
+++ src/com/debasish/practise/final450/FindKthSmallestElementFromArray.java
@@ -1,7 +1,15 @@
+/**
+ * Given an array arr[] and an integer K where K is smaller than size of array,
+ * the task is to find the Kth smallest element in the given array.
+ * It is given that all array elements are distinct.
+ */
 package com.debasish.practise.final450;
 
 import java.util.Arrays;
 
+/**
+ * @author debasishsahoo
+ */
 public class FindKthSmallestElementFromArray {
     public static void main(String[] args) {
         int[] arr = {7, 10, 4, 2, 20, 15};
@@ -9,6 +17,15 @@ public class FindKthSmallestElementFromArray {
         System.out.println("Kth smallest element : " + findKthSmallest(arr, l, r, k));
     }
 
+    /**
+     * Sort the array then find the element by doing search number minus 1.
+     *
+     * @param arr the given input array
+     * @param l   the minimum element to set.
+     * @param r   the maximum element to set.
+     * @param k   the kth element to find.
+     * @return the minimum element.
+     */
     private static int findKthSmallest(int[] arr, int l, int r, int k) {
         Arrays.sort(arr);
         return (arr[k - 1]);
