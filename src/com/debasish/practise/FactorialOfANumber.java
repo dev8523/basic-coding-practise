2ad85e9c76ed2ffb2f1df9a149677aa9a48442fe
dev8523 <debasishsahoo8523@gmail.com>
2021-11-03 23:53:16 +0530

    FactorialOfANumber with recursion

---
+++ src/com/debasish/practise/FactorialOfANumber.java
@@ -0,0 +1,19 @@
+package com.debasish.practise;
+
+import java.util.Scanner;
+
+public class FactorialOfANumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Provide your input: ");
+        System.out.println("Factorial : " + fact(scanner.nextInt()));
+    }
+
+    private static int fact(int input) {
+        int result = 0;
+        if (input == 1) {
+            return input;
+        }
+        return input * fact(input - 1);
+    }
+}
