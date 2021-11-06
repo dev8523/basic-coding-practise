a1f11c975fa9c211d46ad97288bc4610bc94c8da
dev8523 <debasishsahoo8523@gmail.com>
2021-11-06 16:06:41 +0530

    Add proper pseudocode and javadoc for better understanding of the code execution.

--- src/com/debasish/practise/dsa/FindDuplicates.java
+++ src/com/debasish/practise/dsa/FindDuplicates.java
@@ -1,31 +1,57 @@
+/**
+ * WAP to show each word's occurrence in a given string/sentence.
+ * Then show only the duplicate words and their occurrences.
+ */
 package com.debasish.practise.dsa;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * @author debasishsahoo
+ */
 public class FindDuplicates {
     public static void main(String[] args) {
         String inputString = "I am am am learning java java am";
         findDuplicateWord(inputString);
     }
 
+    /**
+     * Pseudo Code: <br/>
+     * 1. Split each word by " ". <br/>
+     * 2. Use a hashmap to store each word and their no. of occurrence.<br/>
+     * 3. For each word in the string: <br/>
+     * 3.1: Check if the word is not null means it is already present in the string: <br/>
+     * 3.1.1: Store the word into the map's key and increment the value by 1. <br/>
+     * 3.2: If word is null/not present already: <br/>
+     * 3.2.1: Add that to the map with value 1. -> When no words will be there initially, then this will be executed. <br/>
+     * 4. Come out of loop and print the map. <br/>
+     * 5. Iterate only the words/keys of the map. <br/>
+     * 6. Check while we have the words in the map: <br/>
+     * 6.1: Extract each word in a temporary variable. <br/>
+     * 6.2: Check if the value of each word is greater than 1 then it means it is DUPLICATED: <br/>
+     * 6.2.1: Print the duplicates. <br/>
+     * <p>
+     *
+     * @param string the input string/sentence.
+     */
     private static void findDuplicateWord(String string) {
-        String[] arr = string.split(" ");
-        Map<String, Integer> map = new HashMap<>();
-        for (String tempString : arr) {
-            if (map.get(tempString) != null) {
-                map.put(tempString, map.get(tempString) + 1);
-            } else {
-                map.put(tempString, 1);
+        String[] arr = string.split(" "); // 1
+        Map<String, Integer> map = new HashMap<>(); // 2
+        for (String tempString : arr) { // 3
+            if (map.get(tempString) != null) { // 3.1
+                map.put(tempString, map.get(tempString) + 1); // 3.1.1
+            } else { // 3.2
+                map.put(tempString, 1); // 3.2.1
             }
         }
-        System.out.println("Showing each word's occurrence: \n" + map + "\n");
-        Iterator<String> keyIterator = map.keySet().iterator();
-        while (keyIterator.hasNext()) {
-            String temp = keyIterator.next();
-            if (map.get(temp) > 1) {
-                System.out.println("The word '" + temp + "' appeared " + map.get(temp) + " no. of times.");
+        System.out.println("Showing each word's occurrence: \n" + map + "\n"); // 4
+        Iterator<String> keyIterator = map.keySet().iterator(); // 5
+        while (keyIterator.hasNext()) { // 6
+            String temp = keyIterator.next(); // 6.1
+            if (map.get(temp) > 1) { // 6.2
+                System.out.println("The word '" + temp + "' appeared " + map.get(temp) + " no. of times."); //6.2.1
             }
         }
     }
