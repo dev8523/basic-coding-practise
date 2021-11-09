81e9f41cf43261af4660220b37eca45959e7d290
dev8523 <debasishsahoo8523@gmail.com>
2021-11-10 00:42:29 +0530

    Print a singly linkedlist

--- src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
+++ src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
@@ -2,4 +2,19 @@ package com.debasish.practise.dsa.linkedlist;
 
 public class SinglyLinkedList {
     private final SinglyLinkedListNode head = null;
+
+    public static void main(String[] args) {
+        SinglyLinkedListNode linkedListNode = new SinglyLinkedListNode(12);
+        printLinkedList(linkedListNode);
+    }
+
+    static void printLinkedList(SinglyLinkedListNode head) {
+        SinglyLinkedListNode current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+        System.out.println("Print the linkedlist: " + head);
+    }
+
 }
