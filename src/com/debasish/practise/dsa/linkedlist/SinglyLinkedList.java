62a226ba9e77f7895477e91258381a3d396a008a
dev8523 <debasishsahoo8523@gmail.com>
2021-11-10 23:10:05 +0530

    Rename Node as SinglyLinkedListNode to differentiate

--- src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
+++ src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
@@ -5,6 +5,8 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
         SinglyLinkedListNode linkedListNode = new SinglyLinkedListNode(12);
+        SinglyLinkedListNode linkedListNode1 = new SinglyLinkedListNode(25);
+        SinglyLinkedListNode linkedListNode2 = new SinglyLinkedListNode(19);
         printLinkedList(linkedListNode);
     }
 
@@ -17,4 +19,23 @@ public class SinglyLinkedList {
         System.out.println("Print the linkedlist: " + head);
     }
 
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode insertableNode = new SinglyLinkedListNode(data);
+        insertableNode.next = llist;
+        return insertableNode;
+    }
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode insertableNode = new SinglyLinkedListNode(data);
+        if (head == null) {
+            head = insertableNode;
+        } else {
+            SinglyLinkedListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = insertableNode;
+        }
+        return head;
+    }
 }
