608e1b198d2da1a320ea2dc165b2066694d233de
dev8523 <debasishsahoo8523@gmail.com>
2021-11-10 23:45:11 +0530

    Insert Nodes at last of a linkedlist and display them

--- src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
+++ src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
@@ -4,19 +4,34 @@ public class SinglyLinkedList {
     private final SinglyLinkedListNode head = null;
 
     public static void main(String[] args) {
-        SinglyLinkedListNode linkedListNode = new SinglyLinkedListNode(12);
-        SinglyLinkedListNode linkedListNode1 = new SinglyLinkedListNode(25);
-        SinglyLinkedListNode linkedListNode2 = new SinglyLinkedListNode(19);
+        SinglyLinkedListNode linkedListNode = new SinglyLinkedListNode(3);
+        insertNodeAtTail(linkedListNode, 9);
+        insertNodeAtTail(linkedListNode, 2);
+        insertNodeAtTail(linkedListNode, 5);
         printLinkedList(linkedListNode);
     }
 
     static void printLinkedList(SinglyLinkedListNode head) {
         SinglyLinkedListNode current = head;
         while (current != null) {
-            System.out.println(current.data);
+            System.out.print(current.data);
+            System.out.print(" -> ");
             current = current.next;
         }
-        System.out.println("Print the linkedlist: " + head);
+        System.out.println("X");
     }
-    
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode insertableNode = new SinglyLinkedListNode(data);
+        if (head == null) {
+            head = insertableNode;
+        } else {
+            SinglyLinkedListNode current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = insertableNode;
+        }
+        return head;
+    }
 }
