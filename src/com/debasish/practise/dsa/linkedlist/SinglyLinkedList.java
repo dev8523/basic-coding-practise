88ef96942fe86006e26311ad8ce236f59f64af07
dev8523 <debasishsahoo8523@gmail.com>
2021-11-11 00:16:01 +0530

    Insert Node at head/start of a linkedlist and display them

--- src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
+++ src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
@@ -5,10 +5,15 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
         SinglyLinkedListNode linkedListNode = new SinglyLinkedListNode(3);
-        insertNodeAtTail(linkedListNode, 9);
-        insertNodeAtTail(linkedListNode, 2);
-        insertNodeAtTail(linkedListNode, 5);
+        linkedListNode = insertNodeAtTail(linkedListNode, 9);
+        linkedListNode = insertNodeAtTail(linkedListNode, 2);
+        linkedListNode = insertNodeAtTail(linkedListNode, 5);
         printLinkedList(linkedListNode);
+
+        SinglyLinkedListNode linkedListNode1 = new SinglyLinkedListNode(10);
+        linkedListNode1 = insertNodeAtHead(linkedListNode1, 40);
+        linkedListNode1 = insertNodeAtHead(linkedListNode1, 60);
+        printLinkedList(linkedListNode1);
     }
 
     static void printLinkedList(SinglyLinkedListNode head) {
@@ -19,6 +24,12 @@ public class SinglyLinkedList {
             current = current.next;
         }
         System.out.println("X");
+    }
+
+    static SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data) {
+        SinglyLinkedListNode insertableNode = new SinglyLinkedListNode(data);
+        insertableNode.next = llist;
+        return insertableNode;
     }
 
     static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
