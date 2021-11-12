a25aae1fd5e1e5926792a0458c6af4abc75c7442
dev8523 <debasishsahoo8523@gmail.com>
2021-11-13 00:53:30 +0530

    Insert a node at any specific position in a linkedlist

--- src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
+++ src/com/debasish/practise/dsa/linkedlist/SinglyLinkedList.java
@@ -7,12 +7,14 @@ public class SinglyLinkedList {
         insertNodeAtTail(linkedListNode, 9);
         insertNodeAtTail(linkedListNode, 2);
         insertNodeAtTail(linkedListNode, 5);
+        insertNodeAtPosition(linkedListNode, 43, 2);
         printLinkedList(linkedListNode);
 
         SinglyLinkedListNode linkedListNode1 = new SinglyLinkedListNode(10);
         linkedListNode1 = insertNodeAtHead(linkedListNode1, 40);
         linkedListNode1 = insertNodeAtHead(linkedListNode1, 60);
         printLinkedList(linkedListNode1);
+
     }
 
     static void printLinkedList(SinglyLinkedListNode head) {
@@ -44,4 +46,19 @@ public class SinglyLinkedList {
         }
         return head;
     }
+
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position) {
+        SinglyLinkedListNode current = llist;
+        SinglyLinkedListNode insertableNode = new SinglyLinkedListNode(data);
+        int i = 1;
+        while (i < position) {
+            i++;
+            current = current.next;
+        }
+        System.out.println("Node after which u want to insert: " + current.data);
+        insertableNode.next = current.next;
+        current.next = insertableNode;
+        return llist;
+    }
+
 }
