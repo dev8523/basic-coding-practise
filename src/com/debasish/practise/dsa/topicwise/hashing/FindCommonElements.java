bc4ab5ce3125ec54d17b6c96e37b40da96c65b63
dev8523 <debasishsahoo8523@gmail.com>
2022-02-13 14:01:40 +0530

    Find CommonElemens in 2 given arrays | Hashing

---
+++ src/com/debasish/practise/dsa/topicwise/hashing/FindCommonElements.java
@@ -0,0 +1,56 @@
+package com.debasish.practise.dsa.topicwise.hashing;
+
+/**
+ * @author debasishsahoo
+ * <p>
+ * Problem Description
+ * <p>
+ * Given two integer array A and B of size N and M respectively. Your task is to find all the common elements in both the array.
+ * <p>
+ * NOTE:
+ * <p>
+ * Each element in the result should appear as many times as it shows in both arrays.
+ * The result can be in any order.
+ * <p>
+ * Input Format
+ * <p>
+ * First argument is an integer array A of size N.
+ * <p>
+ * Second argument is an integer array B of size M.
+ * <p>
+ * Output Format
+ * <p>
+ * Return an integer array denoting the common elements.
+ * <p>
+ * Example Input
+ * <p>
+ * Input 1:
+ * <p>
+ * A = [1, 2, 2, 1]
+ * B = [2, 3, 1, 2]
+ * Input 2:
+ * <p>
+ * A = [2, 1, 4, 10]
+ * B = [3, 6, 2, 10, 10]
+ * <p>
+ * Example Output
+ * <p>
+ * Output 1:
+ * <p>
+ * [1, 2, 2]
+ * Output 2:
+ * <p>
+ * [2, 10]
+ * <p>
+ * Example Explanation
+ * <p>
+ * Explanation 1:
+ * <p>
+ * Elements (1, 2, 2) appears in both the array. Note 2 appears twice in both the array.
+ * Explantion 2:
+ * <p>
+ * Elements (2, 10) appears in both the array.
+ */
+public class FindCommonElements {
+
+}
