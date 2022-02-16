ebba00f734044e18ec2ee555b3890fcb0d13ceb2
dev8523 <debasishsahoo8523@gmail.com>
2022-02-17 01:24:11 +0530

    Find Factorial | Recursion

---
+++ src/com/debasish/practise/dsa/topicwise/recursion/FindFactorial.java
@@ -0,0 +1,18 @@
+package com.debasish.practise.dsa.topicwise.recursion;
+
+/**
+ * @author debasishsahoo
+ * <p>
+ * Problem Description
+ * <p>
+ * Write a program to find the factorial of the given number A.
+ * <p>
+ * Explanation 1:
+ * <p>
+ * Factorial of 4 = 4 * 3 * 2 * 1 = 24
+ * Explanation 2:
+ * <p>
+ * Factorial of 1 = 1
+ */
+public class FindFactorial {
+}
