126bf0b7543645e5ecf640b1773fa88389e7cc03
dev8523 <debasishsahoo8523@gmail.com>
2022-02-19 01:05:42 +0530

    Implement Power Function with modular operation to handle the large values of numbers | Recursion

---
+++ src/com/debasish/practise/dsa/topicwise/recursion/PowerFunctionImpl.java
@@ -0,0 +1,32 @@
+package com.debasish.practise.dsa.topicwise.recursion;
+
+/**
+ * @author debasishsahoo
+ * <p>
+ * Problem Description
+ * <p>
+ * Implement pow(x, n) % d.
+ * In other words, given x, n and d,
+ * <p>
+ * find (xn % d)
+ * <p>
+ * Note that remainders on division cannot be negative. In other words, make sure the answer you return is non negative.
+ * <p>
+ * Input : x = 2, n = 3, d = 3
+ * Output : 2
+ * <p>
+ * 2^3 % 3 = 8 % 3 = 2.
+ */
+public class PowerFunctionImpl {
+    public int pow(int A, int B, int C) {
+        if (A == 0) return 0;
+        if (B == 0) return 1;
+        long x = pow(A, B / 2, C);
+        long ASquare = ((x % C) * (x % C)) % C;
+        if ((B & 1) != 0) {
+            return (int) ((((long) A) * (ASquare) + C) % C);
+        } else {
+            return (int) (ASquare);
+        }
+    }
+}
