19f863ea6a64bf55d932a40a1ada014414c19349
dev8523 <debasishsahoo8523@gmail.com>
2022-02-22 17:03:26 +0530

    Find distinct numbers in sliding window | Hashing - Brute Force approach

---
+++ src/com/debasish/practise/dsa/topicwise/hashing/FindDistinctNumbersInWindow.java
@@ -0,0 +1,71 @@
+package com.debasish.practise.dsa.topicwise.hashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author debasishsahoo
+ * <p>
+ * Problem Description
+ * <p>
+ * You are given an array of N integers, A1, A2 ,…, AN and an integer B. Return the of count of distinct numbers in all windows of size B.
+ * <p>
+ * Formally, return an array of size N-B+1 where i'th element in this array contains number of distinct elements in sequence Ai, Ai+1 ,..., Ai+B-1.
+ * <p>
+ * NOTE: if B > N, return an empty array.
+ * <p>
+ * Example Input
+ * <p>
+ * Input 1:
+ * <p>
+ * A = [1, 2, 1, 3, 4, 3]
+ * B = 3
+ * <p>
+ * Input 2:
+ * <p>
+ * A = [1, 1, 2, 2]
+ * B = 1
+ * <p>
+ * Example Output
+ * <p>
+ * Output 1:
+ * <p>
+ * [2, 3, 3, 2]
+ * <p>
+ * Output 2:
+ * <p>
+ * [1, 1, 1, 1]
+ * <p>
+ * Example Explanation
+ * <p>
+ * Explanation 1:
+ * <p>
+ * A=[1, 2, 1, 3, 4, 3] and B = 3
+ * All windows of size B are
+ * [1, 2, 1]
+ * [2, 1, 3]
+ * [1, 3, 4]
+ * [3, 4, 3]
+ * So, we return an array [2, 3, 3, 2].
+ * <p>
+ * Explanation 2:
+ * <p>
+ * Window size is 1, so the output array is [1, 1, 1, 1].
+ */
+public class FindDistinctNumbersInWindow {
+
+    // TC : O(N * B) and SC : O(N) => Compiler will give TLE error
+    public int[] dNumsBruteForce(int[] A, int B) {
+        int N = A.length;
+        int[] ans = new int[N - B + 1];
+        for (int i = 0; i <= N - B; i++) { // outer loop will iterate over all the starting elements of the sliding window.
+            int j = B + i - 1; // Ending index of the subarray.
+            Set<Integer> set = new HashSet<>();
+            for (int k = i; k <= j; k++) { // iterates over each elements of each sliding window
+                set.add(A[k]);
+            }
+            ans[i] = set.size();
+        }
+        return ans;
+    }
+}
