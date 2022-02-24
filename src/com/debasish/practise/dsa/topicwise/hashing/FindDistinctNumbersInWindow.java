9fc767b2f8604c1103f81ef526964877a5f39740
dev8523 <debasishsahoo8523@gmail.com>
2022-02-24 19:20:53 +0530

    Find distinct number in given sliding window | Hashing

--- src/com/debasish/practise/dsa/topicwise/hashing/FindDistinctNumbersInWindow.java
+++ src/com/debasish/practise/dsa/topicwise/hashing/FindDistinctNumbersInWindow.java
@@ -1,6 +1,8 @@
 package com.debasish.practise.dsa.topicwise.hashing;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -65,6 +67,38 @@ public class FindDistinctNumbersInWindow {
                 set.add(A[k]);
             }
             ans[i] = set.size();
+        }
+        return ans;
+    }
+
+    public int[] dNums(int[] A, int B) {
+        int N = A.length;
+        int[] ans = new int[N - B + 1];
+        //System.out.println(N - B + 1);
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < B; i++) {
+            if (freqMap.containsKey(A[i])) {
+                freqMap.put(A[i], freqMap.get(A[i]) + 1);
+            } else {
+                freqMap.put(A[i], 1);
+                count++;
+            }
+        }
+        ans[0] = count;
+        for (int i = B; i < N; i++) {
+            int j = i - B;
+            if (freqMap.get(A[j]) == 1) {
+                count--;
+            }
+            freqMap.put(A[j], freqMap.get(A[j]) - 1);
+            if (freqMap.containsKey(A[i]) && freqMap.get(A[i]) >= 1) {
+                freqMap.put(A[i], freqMap.get(A[i]) + 1);
+            } else {
+                freqMap.put(A[i], 1);
+                count++;
+            }
+            ans[j + 1] = count;
         }
         return ans;
     }
