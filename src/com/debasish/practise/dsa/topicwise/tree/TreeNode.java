ed2045caffd4fa515bd91915e24848f25812878a
dev8523 <debasishsahoo8523@gmail.com>
2022-02-26 00:30:55 +0530

    Binary Tree Implementation | Trees

---
+++ src/com/debasish/practise/dsa/topicwise/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package com.debasish.practise.dsa.topicwise.tree;
+
+/**
+ * @author debasishsahoo
+ * <p>
+ * This is a binary tree implementation.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
