239c5855e16a9267c8c623ce9b070d512e939787
dev8523 <debasishsahoo8523@gmail.com>
2022-09-12 22:31:14 +0530

    Find N integers containing only 1, 2 & 3

---
+++ src/com/debasish/practise/dsa/topicwise/queue/NIntegersContainingOnly1And2And3.java
@@ -0,0 +1,77 @@
+package com.debasish.practise.dsa.topicwise.queue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Problem Description
+ * Given an integer, A. Find and Return first positive A integers in ascending order containing only digits 1, 2, and 3.
+ * <p>
+ * NOTE: All the A integers will fit in 32-bit integers.
+ * <p>
+ * <p>
+ * <p>
+ * Problem Constraints
+ * 1 <= A <= 29500
+ * <p>
+ * <p>
+ * <p>
+ * Input Format
+ * The only argument given is integer A.
+ * <p>
+ * <p>
+ * <p>
+ * Output Format
+ * Return an integer array denoting the first positive A integers in ascending order containing only digits 1, 2 and 3.
+ * <p>
+ * <p>
+ * <p>
+ * Example Input
+ * Input 1:
+ * <p>
+ * A = 3
+ * Input 2:
+ * <p>
+ * A = 7
+ * <p>
+ * <p>
+ * Example Output
+ * Output 1:
+ * <p>
+ * [1, 2, 3]
+ * Output 2:
+ * <p>
+ * [1, 2, 3, 11, 12, 13, 21]
+ * <p>
+ * <p>
+ * Example Explanation
+ * Explanation 1:
+ * <p>
+ * Output denotes the first 3 integers that contains only digits 1, 2 and 3.
+ * Explanation 2:
+ * <p>
+ * Output denotes the first 3 integers that contains only digits 1, 2 and 3.
+ */
+public class NIntegersContainingOnly1And2And3 {
+    public ArrayList<Integer> solve(int A) {
+        // Using Queue, TC: O(N), SC:O(N)
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        int cnt = 3;
+        while (ans.size() < A) {
+            int x = q.peek();
+            ans.add(x);
+            q.remove();
+            if (cnt >= A) continue;
+            q.add(10 * x + 1);
+            q.add(10 * x + 2);
+            q.add(10 * x + 3);
+            cnt += 3;
+        }
+        return ans;
+    }
+}
