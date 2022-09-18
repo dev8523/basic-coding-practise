9b68df496b05abf6d681296a542b7961be5249bb
dev8523 <debasishsahoo8523@gmail.com>
2022-09-18 22:56:01 +0530

    Check if a binary tree is height balanced

--- src/com/debasish/practise/dsa/topicwise/tree/BalancedHeightBinaryTree.java
+++ src/com/debasish/practise/dsa/topicwise/tree/BalancedHeightBinaryTree.java
@@ -1,17 +1,19 @@
 package com.debasish.practise.dsa.topicwise.tree;
 
 public class BalancedHeightBinaryTree {
+    static boolean result;
+
     private static boolean isHeightBalanced(TreeNode root) {
-        boolean result = true;
-        isBalancedHelper(root, result);
+        result = true;
+        isBalancedHelper(root);
         return result;
     }
 
-    private static int isBalancedHelper(TreeNode root, boolean result) {
+    private static int isBalancedHelper(TreeNode root) {
         if (root == null) return 0;
-        int leftHeight = isBalancedHelper(root.left, result);
-        int rightHeight = isBalancedHelper(root.right, result);
-        if (Math.abs(leftHeight - rightHeight) > 0) result = false;
+        int leftHeight = isBalancedHelper(root.left);
+        int rightHeight = isBalancedHelper(root.right);
+        if (Math.abs(leftHeight - rightHeight) > 1) result = false;
         return 1 + Math.max(leftHeight, rightHeight);
     }
 
@@ -19,7 +21,7 @@ public class BalancedHeightBinaryTree {
      * TC: O(N) - Visiting each node for once.
      * SC: O(N) - recursion stack space.
      */
-    public static int isBalanced(TreeNode A) {
+    public int isBalanced(TreeNode A) {
         return isHeightBalanced(A) ? 1 : 0;
     }
 }
