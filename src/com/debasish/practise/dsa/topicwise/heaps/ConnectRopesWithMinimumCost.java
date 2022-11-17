bde5158d93c8a04afe1e25461b5cd6ced5708ddd
Debasish Sahoo <debasishsahoo8523@gmail.com>
2022-11-17 23:00:52 +0530

    Revise heaps and update doc

--- src/com/debasish/practise/dsa/topicwise/heaps/ConnectRopesWithMinimumCost.java
+++ src/com/debasish/practise/dsa/topicwise/heaps/ConnectRopesWithMinimumCost.java
@@ -5,67 +5,67 @@ import java.util.PriorityQueue;
 /**
  * Problem Description
  * You are given an array A of integers that represent the lengths of ropes.
- *
+ * <p>
  * You need to connect these ropes into one rope. The cost of joining two ropes equals the sum of their lengths.
- *
+ * <p>
  * Find and return the minimum cost to connect these ropes into one rope.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Problem Constraints
  * 1 <= length of the array <= 100000
  * 1 <= A[i] <= 1000
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Input Format
  * The only argument given is the integer array A.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Output Format
  * Return an integer denoting the minimum cost to connect these ropes into one rope.
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example Input
  * Input 1:
- *
- *  A = [1, 2, 3, 4, 5]
+ * <p>
+ * A = [1, 2, 3, 4, 5]
  * Input 2:
- *
- *  A = [5, 17, 100, 11]
- *
- *
+ * <p>
+ * A = [5, 17, 100, 11]
+ * <p>
+ * <p>
  * Example Output
  * Output 1:
- *
- *  33
+ * <p>
+ * 33
  * Output 2:
- *
- *  182
- *
- *
+ * <p>
+ * 182
+ * <p>
+ * <p>
  * Example Explanation
  * Explanation 1:
- *
- *  Given array A = [1, 2, 3, 4, 5].
- *  Connect the ropes in the following manner:
- *  1 + 2 = 3
- *  3 + 3 = 6
- *  4 + 5 = 9
- *  6 + 9 = 15
- *
- *  So, total cost  to connect the ropes into one is 3 + 6 + 9 + 15 = 33.
+ * <p>
+ * Given array A = [1, 2, 3, 4, 5].
+ * Connect the ropes in the following manner:
+ * 1 + 2 = 3
+ * 3 + 3 = 6
+ * 4 + 5 = 9
+ * 6 + 9 = 15
+ * <p>
+ * So, total cost  to connect the ropes into one is 3 + 6 + 9 + 15 = 33.
  * Explanation 2:
- *
- *  Given array A = [5, 17, 100, 11].
- *  Connect the ropes in the following manner:
- *  5 + 11 = 16
- *  16 + 17 = 33
- *  33 + 100 = 133
- *
- *  So, total cost  to connect the ropes into one is 16 + 33 + 133 = 182.
+ * <p>
+ * Given array A = [5, 17, 100, 11].
+ * Connect the ropes in the following manner:
+ * 5 + 11 = 16
+ * 16 + 17 = 33
+ * 33 + 100 = 133
+ * <p>
+ * So, total cost  to connect the ropes into one is 16 + 33 + 133 = 182.
  */
 public class ConnectRopesWithMinimumCost {
     /**
